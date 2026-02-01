842e7fdf830cfc37eb760cbf4ba7f8560118d03f
Olkys-Olivo <olkys.olivogarrido@gmail.com>
2026-02-01 19:30:41 -0400

    Add files via upload

---
+++ Actividad 1 (LR-2024-02080)/Actividad_1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// 1. Declaración de variables con su tipo de dato correspondiente
+string Nombre;
+string Apellido;
+int Edad;
+double Altura;
+
+Console.WriteLine("--- Recopilación de Datos ---");
+
+// Entrada de datos
+
+Console.Write("Coloque su nombre: ");
+Nombre = Console.ReadLine();
+
+Console.Write("Coloque su apellido: ");
+Apellido = Console.ReadLine();
+
+Console.Write("Coloque su edad: ");
+
+// Convertimos la entrada de string a int
+Edad = int.Parse(Console.ReadLine());
+
+Console.Write("Coloque su altura: ");
+
+// Convertimos la entrada de string a decimal double
+Altura = double.Parse(Console.ReadLine());
+
+// Salida de datos
+Console.WriteLine(" ~ Datos Recolectados ~ ");
+
+Console.WriteLine($"Nombre Completo: {Nombre} {Apellido}");
+Console.WriteLine($"Edad:            {Edad} años");
+Console.WriteLine($"Altura:          {Altura} m"); // ejemplo: 1.64 o 1.,64
+
+Console.WriteLine("~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~");
